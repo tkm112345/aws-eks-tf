9fd8e2aa19af11f351384a6a065029fe84092480
tkm112345 <tkm112345@users.noreply.github.com>
2025-12-24 22:23:55 +0900

    chore: format terraform file

--- providers.tf
+++ providers.tf
@@ -1,17 +1,17 @@
 # AWS プロバイダーの設定
 
 provider "aws" {
-  region = "ap-northeast-1"
+  region  = "ap-northeast-1"
   profile = "dev"
 
   # すべてのリソースにデフォルトタグを適用
   default_tags {
     tags = {
-        Environment = var.environment
-        ManagedBy = "terraform"
-        Project = var.project_name
-        Owner = var.owner
-        ConstCenter = var.cost_center
+      Environment = var.environment
+      ManagedBy   = "terraform"
+      Project     = var.project_name
+      Owner       = var.owner
+      ConstCenter = var.cost_center
     }
   }
 }
@@ -22,5 +22,5 @@ data "aws_caller_identity" "current" {}
 
 # 利用可能なアベイラビリティゾーンを取得
 data "aws_availability_zones" "available" {
-    state = "available"
+  state = "available"
 }
